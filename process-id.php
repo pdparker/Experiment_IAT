4fab77e7351b845b76d13d07974ad49906c87160
Philip Parker <phparker@STIPPELO1JYFD59.local>
2014-03-29 17:00:49 +1100

    Remove id and provide 15 random generated string.

--- process-id.php
+++ process-id.php
@@ -1,6 +1,17 @@
 <?php
     session_start();
-    $regName = $_POST['regName'];
+
+//function to generate random code;
+    function generateRandomString($length = 15) {
+      $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+  	  for ($i = 0; $i < $length; $i++) {
+   	       $randomString .= $characters[rand(0, strlen($characters) - 1)];
+ 	          }
+ 		     return $randomString;
+		     }
+
+     $regName = generateRandomString();
     //$_SESSION[‘regName’] = $regName;
     setcookie("id", $regName, time()+3600, '/', NULL, 0 );
     //phpinfo();
